9e0ba61260fd50e1bda254b56f500d5931b3ce03
yilmazfur <furkanyilmaz92@gmail.com>
2021-12-17 23:58:41 +0300

    repos

---
+++ Migrations/20210723103335_Seed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookAPI.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Description", "Title" },
+                values: new object[] { 1, "Victor Hugo", "Les Miserables", null });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Description", "Title" },
+                values: new object[] { 2, "Cervantes", "Don Kişot", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
